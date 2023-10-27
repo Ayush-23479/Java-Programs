007bd39efbb38bf291ccccdaf54354590dac2ab3
Ayush-23479 <122502249+Ayush-23479@users.noreply.github.com>
2023-10-27 10:45:52 +0530

    Update h.java

--- harami.java
+++ harami.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class harami {
     public static void main(String[] args) {
-        System.out.println("Naam Bata ");
+        System.out.println(" Who are you? ");
         Scanner sc=new Scanner(System.in);
         String name = sc.next();
-        System.out.println("Teri Maachod duga " + name + "Bosaridke");
+        System.out.println("Have a nice day" + name + "");
     }
 }
